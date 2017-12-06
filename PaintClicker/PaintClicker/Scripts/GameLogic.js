df0b9d5d0cbe778961048a726c36a82850ff0685
ChrisEllis <CEllis@student.neumont.edu>
2017-12-05 23:29:43 -0700

    Timer updates, meta-data, and metrics

--- PaintClicker/PaintClicker/Scripts/GameLogic.js
+++ PaintClicker/PaintClicker/Scripts/GameLogic.js
@@ -1,17 +1,42 @@
-﻿var paintChips = 0;
-var chisels = 0;
+﻿var paintChips = parseInt(document.getElementById("chipCount").innerHTML);
+var chisels = parseInt(document.getElementById("chisels").innerHTML);
 var chiselPrice = 50;
-var workers = 0;
+var workers = parseInt(document.getElementById("workers").innerHTML);
 var workerPrice = 10;
-var mixers = 0;
+var mixers = parseInt(document.getElementById("mixers").innerHTML);
 var mixerPrice = 25;
-var tickRate = 1000;
+var tickRate = 33;
+var chipsStart = 0
+var chipsEnd = 0;
+var chipsThisSecond = 0;
+
+for (var i = 0; i < chisels; i++)
+{
+    chiselPrice = Math.round(Math.pow(chiselPrice, 1.02));
+}
+
+for (var i = 0; i < workers; i++) {
+    workerPrice = Math.round(Math.pow(workerPrice, 1.07));
+}
+
+for (var i = 0; i < mixers; i++) {
+    mixerPrice = Math.round(Math.pow(mixerPrice, 1.04));
+}
 
 document.getElementById("chipButton").onclick = click;
 document.getElementById("chiselButton").onclick = purchaseChisel;
 document.getElementById("workerButton").onclick = purchaseWorker;
 document.getElementById("mixerButton").onclick = purchaseMixer;
 setInterval(tick, tickRate);
+setInterval(calculateChipsPerSecond, 1000);
+
+function calculateChipsPerSecond()
+{
+    chipsEnd = document.getElementById("chipCount").innerHTML;
+    chipsThisSecond = chipsEnd - chipsStart;
+    document.getElementById("chipsPerSecond").innerHTML = chipsThisSecond;
+    chipsStart = document.getElementById("chipCount").innerHTML;
+}
 
 function click()
 {
@@ -28,15 +53,16 @@ function click()
 }
 
 function tick() {
-    console.log("test");
     var amountToIncrease = 0;
 
     amountToIncrease += workers * 1.2;
     amountToIncrease += mixers * 1;
 
+    amountToIncrease /= 30;
+
     paintChips += amountToIncrease;
 
-    document.getElementById("chipCount").innerHTML = paintChips;
+    document.getElementById("chipCount").innerHTML = Math.round(paintChips);
 }
 
 function purchaseChisel()
