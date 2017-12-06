b74eb31f00e7dfedfa60461ea024a8b05ee3408b
DerekM197 <Derekmikelmartin@gmail.com>
2017-12-05 21:11:23 -0700

    Cleaned up

--- PaintClicker/PaintClicker/Controllers/HomeController.cs
+++ PaintClicker/PaintClicker/Controllers/HomeController.cs
@@ -10,18 +10,23 @@ namespace PaintClicker.Controllers
 {
     public class HomeController : Controller
     {
+        //Port users to DB
+        //Pass user
         public ActionResult Index()
         {
             return View();
         }
 
         
-        public bool Save(string PaintChips, string Chisels, string Workers, string Mixer)
+        public ActionResult Save(int PaintChips, int Chisels, int Workers, int Mixer,string email)
         {
 
 
-            Debug.Write($"\n\n\n\n\n\n Paint:  {PaintChips}  WE SOMEHOW MADE IT HERE\n\n\n\n\n\n");
-            return true;
+            Debug.Write($"\n\n\n\n\n\n Paint:  {PaintChips}  WE SOMEHOW MADE IT HERE\n" +
+                $"Email: {email}" +
+                $"" +
+                $"\n\n\n\n\n");
+            return new EmptyResult();
         }
 
         
